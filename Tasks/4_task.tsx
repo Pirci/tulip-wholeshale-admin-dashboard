0c25d57b2ac06438cce89d2756ce1efb993a5a37
Pirci <emrepirci@gmail.com>
2023-05-16 17:21:00 +0200

    4.1.0

--- Tasks/4_task.tsx
+++ Tasks/4_task.tsx
@@ -1,10 +1,10 @@
-// TASK-3
+// TASK-4
 // The project was prepared for the fourth task by adding react router
 
 // In this task we will make some exercises on props and states.To do that lets make some real examples in our project.
-// Your first objective is to put a conditional view to the Products page. There must be two buttons at the top of the Products Page and under that, there must 
-// be content section. When you cliked the first button you need to see the list of products page. When you clicked the second button, a form should be open 
+// Your first objective is to put a conditional view to the Products page. There must be two buttons at the top of the Products Page and under that, there must
+// be content section. When you cliked the first button you need to see the list of products page. When you clicked the second button, a form should be open
 // which is the product enterance form. Buttons should keep the active state and its background color should be changed accordingly. Please try to use the state
-// mechanism the we studied on Monday. You dont need to spend time to produce a real form or list of objects although it is nice that if you do. But definitely 
-// concantrate on applying of state mechanism. Do not worry about it if you cant. We will go over it on Wednesday. We will also extensively use the state mechanism 
-// in the form components. So, if you put some basic input components and style it, our work become easier. thank you 
+// mechanism the we studied on Monday. You dont need to spend time to produce a real form or list of objects although it is nice that if you do. But definitely
+// concantrate on applying of state mechanism. Do not worry about it if you cant. We will go over it on Wednesday. We will also extensively use the state mechanism
+// in the form components. So, if you put some basic input components and style it, our work become easier. thank you
--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import React, { useState } from "react";
+import styles from "./index.module.scss";
+
+const ProductList = () => (
+  <div className={styles.content_container}>
+    <div className={styles.page_header}>List of Products</div>
+    <ul className={styles.product_list}>
+      <li>Product 1</li>
+      <li>Product 2</li>
+      <li>Product 3</li>
+    </ul>
+  </div>
+);
+
+const ProductEntryForm = () => (
+  <div className={styles.content_container}>
+    <div className={styles.page_header}>Product Entry Form</div>
+    <form className={styles.form_container}>
+      <label>
+        Product Name:
+        <input type='text' name='productName' />
+      </label>
+      <button type='submit'>Submit</button>
+    </form>
+  </div>
+);
 
 export default function Products() {
+  const [activeView, setActiveView] = useState("list");
+
+  const renderContent = () => {
+    if (activeView === "list") {
+      return <ProductList />;
+    } else if (activeView === "form") {
+      return <ProductEntryForm />;
+    }
+  };
+
   return (
-    <div>Products</div>
-  )
+    <div>
+      <div className={styles.button_container}>
+        <button
+          className={activeView === "list" ? styles.active_button : ""}
+          onClick={() => setActiveView("list")}
+        >
+          List of Products
+        </button>
+        <button
+          className={activeView === "form" ? styles.active_button : ""}
+          onClick={() => setActiveView("form")}
+        >
+          Product Entry Form
+        </button>
+      </div>
+      <div>{renderContent()}</div>
+    </div>
+  );
 }
