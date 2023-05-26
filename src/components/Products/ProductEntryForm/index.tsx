53995a653a1457aa755c186eef79a3501e3ce0b8
Pirci <emrepirci@gmail.com>
2023-05-26 13:04:15 +0200

    5.4.0

--- src/components/Products/ProductEntryForm/index.tsx
+++ src/components/Products/ProductEntryForm/index.tsx
@@ -6,7 +6,11 @@ import {
   Radio,
   RadioGroup,
   Snackbar,
+  Stack,
+  Switch,
   TextField,
+  Typography,
+  styled,
 } from '@mui/material';
 import styles from './index.module.scss';
 import { useState } from 'react';
@@ -63,6 +67,51 @@ export const ProductEntryForm = (props: Props) => {
   const handleClose = () => {
     setOpen(false);
   };
+
+  const AntSwitch = styled(Switch)(({ theme }) => ({
+    width: 28,
+    height: 16,
+    padding: 0,
+    display: 'flex',
+    '&:active': {
+      '& .MuiSwitch-thumb': {
+        width: 15,
+      },
+      '& .MuiSwitch-switchBase.Mui-checked': {
+        transform: 'translateX(9px)',
+      },
+    },
+    '& .MuiSwitch-switchBase': {
+      padding: 2,
+      '&.Mui-checked': {
+        transform: 'translateX(12px)',
+        color: '#fff',
+        '& + .MuiSwitch-track': {
+          opacity: 1,
+          backgroundColor:
+            theme.palette.mode === 'dark' ? '#177ddc' : '#1890ff',
+        },
+      },
+    },
+    '& .MuiSwitch-thumb': {
+      boxShadow: '0 2px 4px 0 rgb(0 35 11 / 20%)',
+      width: 12,
+      height: 12,
+      borderRadius: 6,
+      transition: theme.transitions.create(['width'], {
+        duration: 200,
+      }),
+    },
+    '& .MuiSwitch-track': {
+      borderRadius: 16 / 2,
+      opacity: 1,
+      backgroundColor:
+        theme.palette.mode === 'dark'
+          ? 'rgba(255,255,255,.35)'
+          : 'rgba(0,0,0,.25)',
+      boxSizing: 'border-box',
+    },
+  }));
 
   return (
     <div className={styles.content_container}>
@@ -123,15 +172,22 @@ export const ProductEntryForm = (props: Props) => {
         </div>
 
         <div className={styles.form_field}>
-          <TextField
-            id='outlined-basic-7'
-            label='Availablity'
-            variant='outlined'
-            value={formValues.isStockAvailable}
-            onChange={(event) =>
-              handleChange('isStockAvailable', event.target.value)
-            }
-          />
+          <Stack
+            direction='row'
+            spacing={1}
+            alignItems='center'
+            justifyContent='center'
+          >
+            <Typography>False</Typography>
+            <AntSwitch
+              checked={formValues.isStockAvailable}
+              onChange={(event) =>
+                handleChange('isStockAvailable', event.target.checked)
+              }
+              inputProps={{ 'aria-label': 'ant design' }}
+            />
+            <Typography>True</Typography>
+          </Stack>
         </div>
 
         <div className={styles.form_field}>
