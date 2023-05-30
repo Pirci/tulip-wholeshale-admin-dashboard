a833e0b4f222a167f715e0d7f1f557c83d72de13
Harun <harunbcolak@hotmail.com>
2023-05-30 17:01:51 +0300

    5.6.0

--- src/components/Products/ProductList/index.tsx
+++ src/components/Products/ProductList/index.tsx
@@ -7,13 +7,32 @@ import {
   TableCell,
   TableBody,
   TextField,
+  TableFooter,
+  TablePagination,
 } from '@mui/material';
 import styles from './index.module.scss';
 import { products } from '../../../constants/products';
 import { useState } from 'react';
+import TablePaginationActions from '@mui/material/TablePagination/TablePaginationActions';
 
 export const ProductList = () => {
   const [formValues, setFormValues] = useState('');
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   const handleChange = (formValue: string) => {
     console.log(formValue);
@@ -46,34 +65,57 @@ export const ProductList = () => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {products
-                .filter((f) => {
-                  if (formValues.length > 2) {
-                    return f.productName
-                      .toLowerCase()
-                      .includes(formValues.toLowerCase());
-                  } else {
-                    return true;
-                  }
-                })
-                .map((row) => (
-                  <TableRow
-                    key={row.productName}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  >
-                    <TableCell component="th" scope="row">
-                      {row.productName}
-                    </TableCell>
-                    <TableCell align="right">{row.date}</TableCell>
-                    <TableCell align="right">{row.color}</TableCell>
-                    <TableCell align="right">{row.price}</TableCell>
-                    <TableCell align="right">{row.amount}</TableCell>
-                    <TableCell align="right">
-                      {row.isStockAvailable ? 'Exist' : 'Not Exist'}
-                    </TableCell>
-                  </TableRow>
-                ))}
+              {(rowsPerPage > 0
+                ? products
+                    .filter((f) => {
+                      if (formValues.length > 2) {
+                        return f.productName
+                          .toLowerCase()
+                          .includes(formValues.toLowerCase());
+                      } else {
+                        return true;
+                      }
+                    })
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                : products
+              ).map((row) => (
+                <TableRow
+                  key={row.productName}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.productName}
+                  </TableCell>
+                  <TableCell align="right">{row.date}</TableCell>
+                  <TableCell align="right">{row.color}</TableCell>
+                  <TableCell align="right">{row.price}</TableCell>
+                  <TableCell align="right">{row.amount}</TableCell>
+                  <TableCell align="right">
+                    {row.isStockAvailable ? 'Exist' : 'Not Exist'}
+                  </TableCell>
+                </TableRow>
+              ))}
             </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                  colSpan={3}
+                  count={products.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  SelectProps={{
+                    inputProps: {
+                      'aria-label': 'rows per page',
+                    },
+                    native: true,
+                  }}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                  ActionsComponent={TablePaginationActions}
+                />
+              </TableRow>
+            </TableFooter>
           </Table>
         </TableContainer>
       </div>
