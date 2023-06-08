cbaf779d9526541804540b97e93737507cc015a3
Pirci <emrepirci@gmail.com>
2023-06-08 12:18:06 +0200

    7.2.0

--- Tasks/7_task.tsx
+++ Tasks/7_task.tsx
@@ -5,8 +5,8 @@
 // Vendors and customers were put to json-server
 // Modals of vendor and customer were prepared
 
-// In this task, our first easy mission is to setup the react-icons library to our project and use the relevant icons in
-// our left navbar. Here is the official website of the react-icons library https://react-icons.github.io/react-icons/
+// !DONE In this task, our first easy mission is to setup the react-icons library to our project and use the relevant icons in
+// !DONE our left navbar. Here is the official website of the react-icons library https://react-icons.github.io/react-icons/
 
 // Our second mission is to make the vendors and customers pages as products page. We need there table, search and
 // pagination mechanism and customer and vendor add form. Records should be fetched from json server similar to products
@@ -15,8 +15,8 @@
 // Our third mission is prepare our tables for the nested routes. In the next tasks, we will build nested pages for display
 // and edit purpose of the records in the table. To to that, we first need to be able to select table row and you can
 // convert the table to the following one. https://mui.com/material-ui/react-table/#sorting-amp-selecting. Please keep in
-// mind that we will not use the checkbox, so do not add it to table. We just need to track the hover over of records and 
-// also which record is selected. You need to use the useState mechanism for this. Once the record is selected, at the 
+// mind that we will not use the checkbox, so do not add it to table. We just need to track the hover over of records and
+// also which record is selected. You need to use the useState mechanism for this. Once the record is selected, at the
 // bottom right of the table, two buttons should be enabled (normally it is disabled). These buttons are delete and edit.
 // Also, you can move the search box to bottom right for more convenient visual view. Later on we will make this table-style
-// to a seperate component. 
+// to a seperate component.
--- src/components/Layout/Sidebar/index.tsx
+++ src/components/Layout/Sidebar/index.tsx
@@ -4,31 +4,36 @@ import Box from '@mui/material/Box';
 import MuiDrawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import StarIcon from '@mui/icons-material/Star';
-import OutboxIcon from '@mui/icons-material/Outbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
-import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import ShareIcon from '@mui/icons-material/Share';
 import styles from './index.module.scss';
 import { useNavigate } from 'react-router-dom';
+import { AiOutlineDashboard, AiOutlineSetting } from 'react-icons/ai';
+import { BiPurchaseTagAlt } from 'react-icons/bi';
+import {
+  MdOutlineProductionQuantityLimits,
+  MdContentPasteGo,
+  MdArrowForwardIos,
+} from 'react-icons/md';
+import { RiCustomerService2Fill } from 'react-icons/ri';
+import { GiSellCard } from 'react-icons/gi';
 
 const menuItems = [
-  { text: 'Dashboard', icon: <PriorityHighIcon />, route: 'dashboard' },
-  { text: 'Orders', icon: <DraftsIcon />, route: 'orders' },
-  { text: 'Products', icon: <InboxIcon />, route: 'products' },
-  { text: 'Customers', icon: <StarIcon />, route: 'customers' },
-  { text: 'Vendors', icon: <OutboxIcon />, route: 'vendors' },
-  { text: 'Content', icon: <DeleteForeverIcon />, route: 'content' },
-  { text: 'Settings', icon: <ShareIcon />, route: 'settings' },
+  { text: 'Dashboard', icon: <AiOutlineDashboard />, route: 'dashboard' },
+  { text: 'Orders', icon: <BiPurchaseTagAlt />, route: 'orders' },
+  {
+    text: 'Products',
+    icon: <MdOutlineProductionQuantityLimits />,
+    route: 'products',
+  },
+  { text: 'Customers', icon: <RiCustomerService2Fill />, route: 'customers' },
+  { text: 'Vendors', icon: <GiSellCard />, route: 'vendors' },
+  { text: 'Content', icon: <MdContentPasteGo />, route: 'content' },
+  { text: 'Settings', icon: <AiOutlineSetting />, route: 'settings' },
 ];
 
 const drawerWidth = 240;
@@ -138,9 +143,12 @@ export default function Sidebar(props: SideProps) {
         onMouseLeave={startAutoHideTimer}
       >
         <DrawerHeader>
-          <IconButton onClick={handleToggleDrawer}>
+          <MdArrowForwardIos
+            className={styles.arrow}
+            onClick={handleToggleDrawer}
+          >
             {!open ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-          </IconButton>
+          </MdArrowForwardIos>
         </DrawerHeader>
         <Divider />
         <List className={styles.main_container}>
