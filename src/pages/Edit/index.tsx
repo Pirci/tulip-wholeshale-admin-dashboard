4172ace1a4b825dfcc2a134eb31cea47986cba72
Harun <harunbcolak@hotmail.com>
2023-06-27 22:16:59 +0300

    7.10.0

--- src/pages/Edit/index.tsx
+++ src/pages/Edit/index.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Product } from '../../models/product';
+import { CircularProgress } from '@mui/material';
 
 export default function ProductEdit() {
   const { id } = useParams();
+  const [productData, setProductData] = useState<Product>({} as Product);
+  const [loading, setLoading] = useState(true);
 
-  return <div>This is edit page!</div>;
+  useEffect(() => {
+    fetch(`http://localhost:3001/products/${id}`).then((res) => {
+      res.json().then((data) => {
+        setProductData(data);
+        setLoading(false);
+      });
+    });
+  }, [id]);
+
+  if (loading)
+    return (
+      <div>
+        <CircularProgress style={{ color: 'white' }} />
+      </div>
+    );
+
+  return <div>This is edit page product: {productData.productName}</div>;
 }
