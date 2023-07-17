2e66540c2859f809f64fe4031cb37a060b76e9f9
Pirci <emrepirci@gmail.com>
2023-07-17 16:00:21 +0200

    Update 8_task.tsx

---
+++ Tasks/8_task.tsx
@@ -0,0 +1,7 @@
+// TASK-8
+// In edit page on customer, vendor and product, we will show back button for going back to the table page.
+// fetch(`http://localhost:3001/${tableURLExtension}/${selectedItem.id}`, {
+// method: 'PATCH',
+
+// In edit page, button functionality adjusts to the perform the relevant operation.
+// Update or Submit name is changed according to page type.
