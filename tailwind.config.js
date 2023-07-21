59732738b823fad4bcc4d1ceaa553036dc0e5973
Pirci <emrepirci@gmail.com>
2023-07-21 11:32:12 +0200

    8.0.1
    Deploy adjustment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [
+    require('postcss-import'),
+    require('postcss-nesting'), // make sure this comes before tailwind
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+};
