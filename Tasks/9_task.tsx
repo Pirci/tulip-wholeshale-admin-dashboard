4d1453f4540b0fcde482b37aa87e79084e670857
Pirci <emrepirci@gmail.com>
2023-08-02 14:09:03 +0200

    Update 9_task.tsx

--- Tasks/9_task.tsx
+++ Tasks/9_task.tsx
@@ -5,4 +5,8 @@
 // Color name hsould be displayed as hex name in the color observer component when a new color added.
 // layout of the color picker dialog should be fixed.
 // color values should be validated such that they connot be larger than 255 or smaller than 0.
-// Try to migrate all colors to json db, and use it in the color observer component.  
+// Try to migrate all colors to json db, and use it in the color observer component.
+
+// Loading page should be added to the Utilities page. (Loading component should be created)
+// Add toast notification on Utilities page.
+// ?How to trigger fetch mechanism without using setSelected Color?
